9d03b77d2269b555fb4636dfd8a3b51df9c3fd23
Wicky2001 <pmwickramaratna@gmail.com>
2025-08-16 20:52:51 +0530

    pushing to the main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {NavbarComponent} from './component/navbar/navbar.component';
-import {ThemeToggleSwitchComponent} from './component/theme-toggle-switch/theme-toggle-switch.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { ThemeToggleSwitchComponent } from './component/theme-toggle-switch/theme-toggle-switch.component';
 import { HomeComponent } from './component/home/home.component';
+import { FeatureCardComponent } from './component/home/feature-card/feature-card.component';
+import { WorkSectionComponent } from './component/home/work-experience/work-section.component';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +14,8 @@ import { HomeComponent } from './component/home/home.component';
     NavbarComponent,
     ThemeToggleSwitchComponent,
     HomeComponent,
+    FeatureCardComponent,
+    WorkSectionComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
---
+++ src/app/interfaces.ts
@@ -0,0 +1,7 @@
+export default interface WorkExperience {
+  title: string;
+  position: string;
+  type: string;
+  duration: string;
+  description: string;
+}
